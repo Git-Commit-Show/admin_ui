c85d430c98e1e53d5d0f857f54aae435a80d8307
medhachugh <chughmedha1997@gmail.com>
2020-06-20 00:03:18 +0530

    Claps

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,14 @@ var router = express.Router();
 const OBSWebSocket = require('obs-websocket-js');
 var fs = require('fs');
 var path = require('path');
-var root = path.join(__dirname);
+var filePath = path.join(__dirname,'../shared_files/counter.txt');
+var filePath1 = path.join(__dirname,'../shared_files/announcements.txt');
 
 const obs = new OBSWebSocket();
 var scenes;
 var currentScene;
 obs.connect({
-  address: 'https://www.paperspace.com/machine/psaqlqaw3',
+  address: 'localhost:4444',
   password: '0000'
 })
 .then(() => {
@@ -19,6 +20,29 @@ obs.connect({
   console.log(err);
 });
 router.get('/', function(req, res, next) {
+  obs.sendCallback('GetStreamingStatus',{},(err,data)=>{
+    if(data.streaming==false){
+      res.render('index',{StreamingStatus:"Start Streaming",scenes_details:[],currentScene:null});
+    }
+    else{
+      obs.sendCallback('GetSceneList', {}, (err, data) => {
+        if(err)
+        {
+          console.log("Something went wrong!"+" "+err);
+        }
+        else{
+          scenes = data['scenes'];
+          currentScene = data['currentScene'];
+          console.log('ScenesList:',data);
+          res.render('index',{StreamingStatus:"Streaming",scenes_details:scenes,currentScene:currentScene});
+        }
+      });
+    } 
+  })
+});
+router.get('/start',function(req,res,next){
+  obs.sendCallback('StartStreaming',{},(err)=>{
+  })
   obs.sendCallback('GetSceneList', {}, (err, data) => {
     if(err)
     {
@@ -28,33 +52,24 @@ router.get('/', function(req, res, next) {
       scenes = data['scenes'];
       currentScene = data['currentScene'];
       console.log('ScenesList:',data);
-      res.render('index',{StreamingStatus:"Start Streaming",scenes_details:[],currentScene:null});
+      res.render('index',{StreamingStatus:"Streaming",scenes_details:scenes,currentScene:currentScene});
     }
-  })
-});
-router.get('/start',function(req,res,next){
-  
-  obs.sendCallback('StartStreaming', (error) => {
   });
-  res.render('index',{"StreamingStatus":"Streaming",scenes_details:scenes,scenes_details:scenes,currentScene:currentScene});
-  
 });
 
 router.get('/stop',function(req,res,next){  
   obs.sendCallback('StopStreaming', (error) => {
     console.log("stopped working");
   });
-  res.redirect('/');
+  res.render('index',{"StreamingStatus":"Start Streaming",scenes_details:scenes,scenes_details:scenes,currentScene:"MasterClass"});
 });
 
 
 
 router.get('/masterclass',function(req,res,next){
-
-   obs.send('SetCurrentScene', {
-    'scene-name': 'MasterClass'
-});
-
+      obs.send('SetCurrentScene', {
+        'scene-name': 'MasterClass'
+    });
 res.render('index',{"StreamingStatus":"Streaming",scenes_details:scenes,scenes_details:scenes,currentScene:"MasterClass"});
 });
 router.get('/showcase',function(req,res,next){
@@ -79,21 +94,11 @@ router.get('/counter',function(req,res,next){
   obs.send('SetCurrentScene', {
     'scene-name': 'stats'   
 });
-
-/*fs.readFile(root+'‪\counter.txt',function(err,data){
-   fs.writeFile(root+'\counter.txt',data+1, function (err) {
-    if (err) throw err;
-    console.log('Replaced!');
-  });});*/
-  console.log('File contents:' + Number(fs.readFileSync(root+'counter.txt')));
-  //console.log(t);
-  fs.readFile(root+'counter.txt', 'utf-8', function(err, data) { 
-    // Write the data read from readeMe.txt 
-    // to a file writeMe.txt
+  fs.readFile(filePath, 'utf-8', function(err, data) { 
     if( !err ) 
         {
           console.log(data); 
-        fs.writeFile(root+'counter.txt‪', data, (err)=>{ 
+        fs.writeFile(filePath, parseInt(data)+1, (err)=>{ 
             if( err ) { 
                 throw err; 
             } 
@@ -102,6 +107,17 @@ router.get('/counter',function(req,res,next){
         {throw err; }
 }); 
 
-res.render('index',{"StreamingStatus":"Streaming",scenes_details:scenes,scenes_details:scenes,currentScene:"Break"});
+res.render('index',{"StreamingStatus":"Streaming",scenes_details:scenes,scenes_details:scenes,currentScene:"stats"});
+});
+router.all('/post',function(req,res,next){
+  obs.send('SetCurrentScene', {
+    'scene-name': 'stats'   
+});
+  fs.writeFile(filePath1,req.body.announcements, (err)=>{ 
+    if(err){
+      console.log(err);
+    }
+});
+res.render('index',{"StreamingStatus":"Streaming",scenes_details:scenes,scenes_details:scenes,currentScene:"stats"});
 });
 module.exports = router;
--- routes/trial.js
+++ routes/trial.js
@@ -1,14 +1,2 @@
-const fs = require('fs');
-var path = require('path');
-var root = path.join(__dirname);
-
-fs.readFile(root+'counter.txt',"utf-8",(err,data)=>{
-    if(err){
-        console.log(err);
-    }
-    else{
-         console.log(data);
-
-    }
-
-});
+var alert = require('alert');
+alert("Is it how it works");
